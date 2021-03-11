09b557f4a17894d9533da4dfc686d72a9fd5cbfb
chenzhihui <zhihui_chen@foxmail.com>
2021-03-11 23:13:29 +0800

    init

--- registry/registry_test.go
+++ registry/registry_test.go
@@ -1,67 +1,72 @@
 package registry
 
 import (
+	"context"
 	"testing"
 	"time"
 
-	"github.com/coreos/etcd/Godeps/_workspace/src/golang.org/x/net/context"
 	"github.com/go-kratos/kratos/v2/registry"
-	"github.com/stretchr/testify/assert"
-	"go.etcd.io/etcd/clientv3"
+	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
-func TestRegister(t *testing.T) {
+func TestRegistry(t *testing.T) {
 	conf := clientv3.Config{}
 	conf.Endpoints = []string{"127.0.0.1:2379"}
-	etcdCli, err := clientv3.New(conf)
-	assert.Nil(t, err)
-	register := New(etcdCli)
-	service1 := &registry.ServiceInstance{}
-	service1.Name = "test"
-	service1.ID = "1"
-	err = register.Register(context.Background(), service1)
-	assert.Nil(t, err)
-	watch, err := register.Watch(context.Background(), "test")
-	assert.Nil(t, err)
+	client, err := clientv3.New(conf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer client.Close()
+
+	ctx := context.Background()
+	s := &registry.ServiceInstance{
+		ID:   "0",
+		Name: "helloworld",
+	}
+
+	r := New(client)
+	w, err := r.Watch(ctx, s.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer w.Stop()
+	go func() {
+		for {
+			res, err := w.Next()
+			if err != nil {
+				return
+			}
+			t.Logf("watch: %d", len(res))
+			for _, r := range res {
+				t.Logf("next: %+v", r)
+			}
+		}
+	}()
 	time.Sleep(time.Second)
 
-	ss, err := watch.Next()
-	assert.Nil(t, err)
-	assert.Len(t, ss, 1)
+	if err := r.Register(ctx, s); err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(time.Second)
 
-	service2 := &registry.ServiceInstance{}
-	service2.Name = "test"
-	service2.ID = "2"
-	err = register.Register(context.Background(), service2)
+	res, err := r.Service(ctx, s.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(res) != 1 && res[0].Name != s.Name {
+		t.Errorf("not expected: %+v", res)
+	}
 
-	ss, err = watch.Next()
-	assert.Nil(t, err)
-	assert.Len(t, ss, 2)
+	if err := r.Deregister(ctx, s); err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(time.Second)
 
-	ss, err = register.Fetch(context.Background(), "test")
-	assert.Nil(t, err)
-	assert.Len(t, ss, 2)
-	service3 := &registry.ServiceInstance{}
-	service3.Name = "test"
-	service3.ID = "3"
-	err = register.Register(context.Background(), service3)
-
-	ss, err = watch.Next()
-	assert.Nil(t, err)
-	assert.Len(t, ss, 3)
-
-	ss, err = register.Fetch(context.Background(), "test")
-	assert.Nil(t, err)
-	assert.Len(t, ss, 3)
-
-	err = register.Deregister(context.Background(), service3)
-	assert.Nil(t, err)
-
-	ss, err = watch.Next()
-	assert.Nil(t, err)
-	assert.Len(t, ss, 2)
-
-	ss, err = register.Fetch(context.Background(), "test")
-	assert.Nil(t, err)
-	assert.Len(t, ss, 2)
+	res, err = r.Service(ctx, s.Name)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(res) != 0 {
+		t.Errorf("not expected empty")
+	}
 }
